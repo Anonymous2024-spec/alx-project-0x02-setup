34f7e97650d6f3a7fd3425be2d506ba8b85eaf6c
Naana Shifah <shifahnana2@gmail.com>
2025-07-24 15:29:25 +0300

    fix: change code in the interface - index

--- alx-project-0x02/interfaces/index.tsx
+++ alx-project-0x02/interfaces/index.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
-const index = () => {
-  return <div>index</div>;
-};
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
 
-export default index;
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
+export interface UserModalProps {
+  onClose: () => void;
+  onSubmit: (post: UserProps) => void;
+}
