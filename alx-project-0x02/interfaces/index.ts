5428c9080c669f3127f0ae9157a40482e5565442
Naana Shifah <shifahnana2@gmail.com>
2025-07-25 16:17:14 +0300

    feat: create and use a button component

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -7,3 +7,11 @@ export interface PostModalProps {
   onClose: () => void;
   onSubmit: (post: PostData) => void;
 }
+
+export interface ButtonProps {
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  children: React.ReactNode;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+}
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button"
 
 const AboutPage: React.FC = () => {
   return (
     <div>
       <Header />
-      <h1 className="p-50 text-2xl font-bold text-center">About Page</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
+      <Button size="small" shape="rounded-sm">Small Button</Button>
+      <Button size="medium" shape="rounded-md">Medium Button</Button>
+      <Button size="large" shape="rounded-full">Large Button</Button>
+    </div>
     </div>
   );
 };
